139bb6bc9c7bf3433edea28f0ceb13fff94c6cd5
Horace <horaceyeh@outlook.com>
2023-02-12 01:07:40 +0800

    vue專案 調整為npm init vue@latest 建立

--- dotnet7vue3SpaSimple/dotnet7vue3SpaSimple/ClientApp/vite.config.js
+++ dotnet7vue3SpaSimple/dotnet7vue3SpaSimple/ClientApp/vite.config.js
@@ -1,21 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), mkcert()],
-    server: {
-        port: 44417,
-        https: true,
-        strictPort: true,
-        proxy: {
-            '/api': {
-                target: 'https://localhost:7208',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, '/api')
-            }
-        }
+  plugins: [vue(), mkcert()],
+  server: {
+    port: 44417,
+    https: true,
+    strictPort : true,
+    proxy: {
+      '/api' : {
+        target: 'https://localhost:7208',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    } 
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
 })
